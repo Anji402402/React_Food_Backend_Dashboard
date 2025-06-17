71ec4750d9c61ea7f3cbc98c777845331b1056b8
Anji402 <kadalianji402@gmail.com>
2025-06-17 14:05:38 +0530

    changed path  to live in api_url

--- src/vendorDashaboard/data/apiPath.js
+++ src/vendorDashaboard/data/apiPath.js
@@ -1,2 +1,2 @@
 // export const API_URL = "http://localhost:5000"
-export const API_URL ="https://backend-nodejs-anji.onrender.com"
+export const API_URL ="https://backend-nodejs-anjaneyulu.onrender.com"
